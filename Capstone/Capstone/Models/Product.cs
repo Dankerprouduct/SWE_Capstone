d7b1170457b32112fc69310c7ac10f5c45020267
jblake33 <jcb1354@gmail.com>
2022-11-07 18:44:21 -0500

    start dark/light mode
    started on a working dark/light mode toggle

--- Capstone/Capstone/Models/Product.cs
+++ Capstone/Capstone/Models/Product.cs
@@ -8,9 +8,11 @@ namespace Capstone.Models
 {
     public class Product
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Price { get; set; }
-
+        public string Name { get; set; }        //The name of each product
+        public string Description { get; set; } //unused?
+        public string Price { get; set; }       //The price of each product (USD, don't worry about other currencies)
+        public string Retailer { get; set; }    //The name of the retailer where each of the products was scraped from
+        public byte[] Image { get; set; }       //The image that show up next to each of the products in search results
+        public string ExternalLink { get; set; }//The link to the retailer's listing page where each item is sold
     }
 }
--- Capstone/Capstone/SettingsData.cs
+++ Capstone/Capstone/SettingsData.cs
@@ -9,11 +9,13 @@ namespace Capstone
 {
     public class SettingsData : INotifyPropertyChanged
     {
+        //Data binding for the settings, so it is saved between windows.
         public SettingsData()
         {
             //
         }
-        //Data binding for the settings so it is saved between windows.
+        
+        //Filter by a specific retailer (one variable for each).
         bool isAmazonChecked;
         public bool IsAmazonChecked
         {
@@ -45,13 +47,44 @@ namespace Capstone
             set { isTargetChecked = value; OnPropertyChanged(nameof(IsTargetChecked)); }
         }
 
+        //Should search data be saved and sent to the DB? (settings)
         bool isSaveDataChecked;
         public bool IsSaveDataChecked {
             get { return isSaveDataChecked; }
             set { isSaveDataChecked = value; OnPropertyChanged(nameof(IsSaveDataChecked)); }
         }
 
-        
+        //Is the dark mode enabled? False = light mode (settings)
+        bool isDarkModeChecked;
+        public bool IsDarkModeChecked
+        {
+            get { return isDarkModeChecked; }
+            set { isDarkModeChecked = value; OnPropertyChanged(nameof(IsSaveDataChecked)); }
+        }
+
+        //Does the user want price drop notifications? (settings)
+        bool isPriceAlertsEnabled;
+        public bool IsPriceAlertsEnabled
+        {
+            get { return isPriceAlertsEnabled; }
+            set { isPriceAlertsEnabled = value; OnPropertyChanged(nameof(IsPriceAlertsEnabled)); }
+        }
+
+        //user's phone number (settings)
+        string _phone;
+        public string Phone
+        {
+            get { return _phone; } 
+            set { _phone = value; OnPropertyChanged(nameof(Phone)); }
+        }
+
+        //user's email address (settings)
+        string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; OnPropertyChanged(nameof(Email)); }
+        }
 
         public event PropertyChangedEventHandler? PropertyChanged;
         void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
